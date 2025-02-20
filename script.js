d148302880c7f6c408cd8feebadd910617304152
Deepak Yadav <deepak160895@gmail.com>
2025-02-20 22:16:42 +0530

    TODO APP

---
+++ script.js
@@ -0,0 +1,84 @@
+const input = document.querySelector("input")
+const taskContainer = document.querySelector("ul")
+const addBtn = document.getElementById("add-btn")
+
+addBtn.addEventListener("click", () => {
+    let value = input.value
+
+    if (!value) {
+        return alert("Are bhaiya, Pahle TODO to Daldo")
+    }
+
+    const li = document.createElement("li")
+    li.classList.add("todo-item")
+
+    const buttonBox = document.createElement("div")
+    buttonBox.classList.add("button-box")
+
+    const editButton = document.createElement("button")
+    const deleteButton = document.createElement("button")
+
+    editButton.innerText = "edit"
+    editButton.classList.add("list-btn", "edit")
+    deleteButton.innerText = "del"
+    deleteButton.classList.add("list-btn", "del")
+
+    buttonBox.append(editButton)
+    buttonBox.append(deleteButton)
+
+    li.innerText = value
+    li.append(buttonBox)
+
+    taskContainer.append(li)
+
+    input.value = ""
+
+    //delete functionality 
+
+    deleteButton.addEventListener("click", (e) => {
+        e.target.closest("li").remove()
+
+    })
+
+    //edit functionality 
+
+    editButton.addEventListener("click", (e) => {
+        const li = e.target.closest("li")
+        const input = document.createElement("input")
+        input.type = "text"
+        input.classList.add("update-input")
+
+        const existingContent = li.firstChild.textContent
+
+        input.value = existingContent
+
+        const saveBtn = document.createElement("button")
+        saveBtn.classList.add("list-btn", "save")
+        saveBtn.innerText = "save"
+
+        li.innerHTML = ""
+        li.append(input)
+        li.append(saveBtn)
+
+        // Save Functionality 
+
+        saveBtn.addEventListener("click", (e) => {
+            const li = e.target.closest("li")
+            const updatedText = li.firstChild.value
+            if (updatedText) {
+                li.innerText = updatedText
+                li.append(buttonBox)
+            } else {
+                alert("Kuch to dalo usmien")
+            }
+        })
+
+    })
+
+
+
+
+
+
+
+})
